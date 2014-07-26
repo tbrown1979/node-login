1318e9ecd998ac455320fe47341845fb4c7e7db5
tbrown1979 <tbrown@uni.edu>
2014-07-25 20:20:12 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var express  = require('express');
+var app      = express();
+var path     = require('path');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var logger       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url);
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(bodyParser());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//passport
+app.use(session({ secret: 'terp' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+//routes returns function
+var routes = require('./routes/index')();
+app.use('/', routes);
+
+module.exports = app;
